e853281550a9ee0245bbf5bca939a938cb1f808a
jeffbuenting <jeffbuenting@gmail.com>
2016-08-12 15:15:06 -0400

    SSRS cmdlets
    beginning

---
+++ RawCode/Get-SSRSReport.ps1
@@ -0,0 +1,53 @@
+﻿Function Get-SSRSReport {
+
+<#
+    .Synopsis
+        Gets a list of SSRS Reports ( RDL File ) on the SQL SSRS Server
+
+    .Link
+        The majority of this script came from this website
+
+        http://www.geoffhudik.com/tech/2011/10/13/uploading-ssrs-reports-with-powershell.html
+        https://blogs.infosupport.com/managing-ssrs-reports-with-powershell/
+#>
+
+    [CmdletBinding()]
+    Param (
+        [string]$SSRSServer,
+
+        [String]$Report
+    )
+
+    Begin {
+        Write-Verbose "Connecting to $SSRSServer"
+        $reportServerUri = "http://$SSRSServer/reportserver/ReportService2010.asmx?wsdl"
+        $RS = New-WebServiceProxy -Uri $reportServerUri -Namespace SSRS -UseDefaultCredential
+        #$RS | GM
+
+        # ----- Create property for the FindItems method
+        $Property = New-Object ReportService2010.Property
+        $Property.Name = 'Recurse'
+        $Property.Value = $True
+    }
+
+    Process {
+        # Download all Reports from a specific folder to .rdl files in the current 
+        # directory.
+        
+        #$items = $RS.ListChildren($sourceFolderPath, $false)
+        $Reports = $RS.FindItems( "/","AND",$Propery,"")
+        Write-Output $Reports
+        
+        #Write-Output ($items | Where-Object { $_.TypeName -eq "Report" } )
+        
+        #| Foreach-Object {
+        #    $filename = ("{0}.rdl" -f $_.Name)
+
+        #    Write-Output ("Downloading ""{0}""..." -f $_.Path)
+        #    $bytes = $RS.GetItemDefinition($_.Path)
+        #    [System.IO.File]::WriteAllBytes("$pwd\$filename", $bytes)
+        #}
+    }
+}
+
+Get-SSRSReport -SSRSServer jeffb-sql01.stratuslivedemo.com -verbose
---
+++ RawCode/Upload-SSRSReport.ps1
@@ -0,0 +1,31 @@
+﻿Function Upload-SSRSReport {
+
+<#
+    .Synopsis
+        Uploads an SSRS Report ( RDL File ) to SQL SSRS Server
+
+    .Link
+        The majority of this script came from this website
+
+        http://www.geoffhudik.com/tech/2011/10/13/uploading-ssrs-reports-with-powershell.html
+#>
+
+    [CmdletBinding()]
+    Param (
+        [string]$SSRSServer,
+
+        [String]$Report
+    )
+
+    Begin {
+        Write-Output "Connecting to $SSRSServer"
+        $reportServerUri = "http://$SSRSServer/ReportServer/ReportService2005.asmx"
+        $proxy = New-WebServiceProxy -Uri $reportServerUri -Namespace SSRS.ReportingService2005 -UseDefaultCredential
+    }
+
+    Process {
+        Foreach ( $R in $Report ) {
+            
+        }
+    }
+}
