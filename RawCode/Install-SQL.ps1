24b319f85f50e8a7e2000d4ba437b2addbc9ed16
jeffbuenting <jeffbuenting@gmail.com>
2016-10-18 15:47:21 -0400

    Install-SQL completed

--- RawCode/Install-SQL.ps1
+++ RawCode/Install-SQL.ps1
@@ -50,7 +50,20 @@
         Path to where the DB Log files will be stored
 
     .Parameter Credential
-        User name and password of an account with permissions required to install SQL        
+        User name and password of an account with permissions required to install SQL  
+        
+    .Example
+        Installs SQL on ServerA
+        
+        install-SQLServer -ComputerName ServerA `
+            -SQLISO c:\iso\en_sql_server_2014_developer_edition_with_service_pack_1_x64_dvd_6668542.iso `
+            -Features SQLEngine,FullText,RS,SSMS,ADV_SSMS `
+            -SQLAgent $SQLSVCAccount `
+            -SAPWD $SAWPD `
+            -SQLService $SQLSVCAccount `
+            -SysAdmin 'Contoso\ServerA-SQLAdmins' `
+            -ReportService $SQLSVCAccount `
+            -Credential $CRMAdmin   
 
     .Link
         Explaination of SQL Commandline switches
@@ -61,6 +74,10 @@
         Why CredSSP is required to install SQL remotely
 
         https://social.technet.microsoft.com/Forums/en-US/ea696b2f-39a0-44d0-9121-8a653c34e4a8/installing-sql-server-2012-remotely-via-powershell-using-invokecommand?forum=ITCG
+
+    .Notes
+        Author : Jeff Buetning
+        Date : 2016 OCT 18
 
 #>
 
@@ -146,7 +163,7 @@
         }
 
         Write-Verbose "Installing SQL on $ComputerName"
-        Invoke-Command -ComputerName $ComputerName -Authentication Credssp -Credential $Credential -Scriptblock {
+        $SummaryLog = Invoke-Command -ComputerName $ComputerName -Authentication Credssp -Credential $Credential -Scriptblock {
             
             #----- Set verbose pref to what calling shell is set to
             $VerbosePreference=$Using:VerbosePreference
@@ -162,68 +179,68 @@
             $SAWPDAcct = $Using:SAWPD
 
             # ----- Pulling the argument list out from the Start-Process cmdlet to allow different arguments added depending on parameters.
-            #$ArgumentList = "/ACTION=Install /IAcceptSQLServerLicenseTerms /ENU=True /QUIET=True /UpdateEnabled=True /ERRORREPORTING=False /USEMICROSOFTUPDATE=True /UpdateSource=MU /FEATURES=$Using:Features /HELP=False /INDICATEPROGRESS=False /X86=False /INSTALLSHAREDDIR=$Using:InstallSharedDir /INSTALLSHAREDWOWDIR='C:\Program Files (x86)\Microsoft SQL Server' /INSTANCENAME=$Using:InstanceName /SQMREPORTING=False /INSTANCEID=$Using:InstanceID /RSINSTALLMODE=DefaultNativeMode /INSTANCEDIR=$Using:InstallSQLDataDir /AGTSVCSTARTUPTYPE=Automatic /SQLSVCSTARTUPTYPE=Automatic /BROWSERSVCSTARTUPTYPE=Disabled /RSSVCSTARTUPTYPE=Automatic /FTSVCACCOUNT=NT Service\MSSQLFDLauncher /SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS /SQLSYSADMINACCOUNTS=$Using:SysAdmin /TCPENABLED=1 /NPENABLED=1"
             $ArgumentList = "/ACTION=Install /IAcceptSQLServerLicenseTerms /ENU=True /QUIET=True /UpdateEnabled=True /ERRORREPORTING=False /USEMICROSOFTUPDATE=True /UpdateSource=MU /FEATURES=SQLEngine FullText RS SSMS ADV_SSMS /HELP=False /INDICATEPROGRESS=False /X86=False  /INSTANCENAME=MSSQLSERVER /SQMREPORTING=False /INSTANCEID=MSSQLSERVER /RSINSTALLMODE=DefaultNativeMode  /AGTSVCSTARTUPTYPE=Automatic /SQLSVCSTARTUPTYPE=Automatic /BROWSERSVCSTARTUPTYPE=Disabled /RSSVCSTARTUPTYPE=Automatic /FTSVCACCOUNT=NT Service\MSSQLFDLauncher /SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS /SQLSYSADMINACCOUNTS=stratuslivedemo\jeff.buenting stratuslivedemo\administrator /TCPENABLED=1 /NPENABLED=1 "
 
 
-            # ----- Known accounts Local System, Network Service, etc, when passed as PSCredential only have the user name not the 'domain'.  This section adds the domain if necessary
- #           if ( $SQLServiceAcct.UserName -eq 'Network Service' ) {
- #                   $ArgumentList += " /SQLSVCACCOUNT='NT AUTHORITY\NETWORK SERVICE'"
- #               }
- #               else {
- #                   $ArgumentList += " /SQLSVCACCOUNT=$($SQLServiceAcct.UserName)"
- #           }
- #           if ( $SQLAgentAcct.UserName -eq 'Network Service' ) {
- #                   $ArgumentList += " /AGTSVCACCOUNT='NT AUTHORITY\NETWORK SERVICE'"
- #               }
- #               else {
-  #                  $ArgumentList += " /AGTSVCACCOUNT=$($SQLAgentAcct.UserName)"
- #           }
- #           if ( $SQLAgentAcct.UserName -eq 'Network Service' ) {
- #                   $ArgumentList += " /RSSVCACCOUNT='NT AUTHORITY\NETWORK SERVICE'"
- #               }
- #               else {
- #                   $ArgumentList += " /RSSVCACCOUNT=$($ReportServiceAcct.UserName)"
- #           }
+            # ----- Add User name.  If no name is provided, SQL will use its defaults
+            if ( $SQLServiceAcct.UserName ) { $ArgumentList += " /SQLSVCACCOUNT=$($SQLServiceAcct.UserName)" }
+            if ( $SQLAgentAcct.UserName  ) { $ArgumentList += " /AGTSVCACCOUNT=$($SQLAgentAcct.UserName)" }
+            if ( $SQLAgentAcct.UserName ) { $ArgumentList += " /RSSVCACCOUNT=$($ReportServiceAcct.UserName)" }
 
             # ----- Adds the password if one is passed.
             if ( $SQLServiceAcct.GetNetworkCredential().Password ) { $ArgumentList += " /SQLSVCPassword=$($SQLServiceAcct.GetNetworkCredential().Password)" }
-            if ( $SQLAgentAcct.GetNetworkCredential().Password ) { $ArgumentList += " /AGTSVCPassword = $($SQLAgentAcct.GetNetworkCredential().Password)" }
+            if ( $SQLAgentAcct.GetNetworkCredential().Password ) { $ArgumentList += " /AGTSVCPassword=$($SQLAgentAcct.GetNetworkCredential().Password)" }
             if ( $ReportServiceAcct.GetNetworkCredential().Password ) { $ArgumentList += " /RSSVCPASSWORD=$($ReportServiceAcct.GetNetworkCredential().Password)" }
             if ( $SAWPDAcct.GetNetworkCredential().Password ) { $ArgumentList += " /SECURITYMODE=SQL /SAPWD=$($SAWPDAcct.GetNetworkCredential().Password)" }
 
             Write-verbose "Beginning the Install"
             Write-Verbose "Install Arguments = $ArgumentList"
-        #    start-Process -FilePath "E:\Setup.exe"  -ArgumentList "/ACTION=Install /Q /UpdateEnabled=True /FEATURES=$Using:Features /InstallShareDDir=$Using:InstallSharedDir /SQMREPORTING=0 /AGTSVCACCOUNT=$SQLAGENTacct.Username /AGTSVCPASSWORD=$SQLAgentAcct.GetNetworkCredential().Password /AGTSVCSTARTUPType=Automatic /InstallSQLDATADIR=$Using:INSTALLSQLDATADIR /SAPWD=$Using:SAPWD /SECURITYMODE=SQL /SQLSVCACCOUNT=$SQLServiceAcct.Username /SQLSVCPASSWORD=$SQLServiceAcct.GetNetworkCredential().Password /SQLSVCSTARTUPTYPE=Automatic /SQLSYSADMINACCOUNTS=$Using:SYSAdmin /SQLTEMPDBDIR=$Using:SQLTempDBDir /SQLTEMPDBLOGDIR=$Using:SQLTempDBLogDir /SQLUSERDBDIR=$Using:SQLUserDBDir /SQLUSERDBLOGDIR=$Using:SQLUserDBLogDir /NPEnabled=1 /RSSVCACCOUNT=$ReportServiceAcct.Username /RSSVCPASSWORD=$ReportServiceAcct.GetNetworkCredential().Password /RSSVCSTARTUPTYPE=Automatic /IAcceptSQLServerLicenseTerms" -wait -Credential $Using:Credential
-            #start-Process -FilePath "e:\Setup.exe"  -ArgumentList '/ACTION=Install /IAcceptSQLServerLicenseTerms /ENU=True /QUIET=True /UpdateEnabled=True /ERRORREPORTING=False /USEMICROSOFTUPDATE=True /UpdateSource=MU /FEATURES=SQLENGINE,FULLTEXT,RS,SSMS,ADV_SSMS /HELP=False /INDICATEPROGRESS=False /X86=False /INSTALLSHAREDDIR="C:\Program Files\Microsoft SQL Server" /INSTALLSHAREDWOWDIR="C:\Program Files (x86)\Microsoft SQL Server" /INSTANCENAME=MSSQLSERVER /SQMREPORTING=False /INSTANCEID=MSSQLSERVER /RSINSTALLMODE=DefaultNativeMode /INSTANCEDIR="C:\Program Files\Microsoft SQL Server" /AGTSVCACCOUNT="NT AUTHORITY\NETWORK SERVICE" /AGTSVCSTARTUPTYPE=Automatic /SQLSVCACCOUNT="NT AUTHORITY\NETWORK SERVICE" /SQLSVCSTARTUPTYPE=Automatic /BROWSERSVCSTARTUPTYPE=Disabled /RSSVCACCOUNT="NT AUTHORITY\NETWORK SERVICE" /RSSVCSTARTUPTYPE=Automatic /FTSVCACCOUNT=NT Service\MSSQLFDLauncher /SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS /SQLSYSADMINACCOUNTS=STRATUSLIVEDEMO\Jeff.Buenting /SECURITYMODE=SQL /SAPWD=Password1 /TCPENABLED=0 /NPENABLED=1' -wait -Credential $Using:Credential
-            
-           
-           # start-Process -FilePath "$($DriveLetter):\Setup.exe"  -ArgumentList "/ACTION=Install /IAcceptSQLServerLicenseTerms /ENU=True /QUIET=True /UpdateEnabled=True /ERRORREPORTING=False /USEMICROSOFTUPDATE=True /UpdateSource=MU /FEATURES=$Using:Features /HELP=False /INDICATEPROGRESS=False /X86=False /INSTALLSHAREDDIR=$Using:InstallSharedDir /INSTALLSHAREDWOWDIR='C:\Program Files (x86)\Microsoft SQL Server' /INSTANCENAME=$Using:InstanceName /SQMREPORTING=False /INSTANCEID=$Using:InstanceID /RSINSTALLMODE=DefaultNativeMode /INSTANCEDIR=$Using:InstallSQLDataDir /AGTSVCACCOUNT=$($SQLAgentAcct.UserName) /AGTSVCPassword = $($SQLAgentAcct.GetNetworkCredential().Password) /AGTSVCSTARTUPTYPE=Automatic /SQLSVCACCOUNT=$($SQLServiceAcct.UserName) /SQLSVCPassword=$($SQLServiceAcct.GetNetworkCredential().Password) /SQLSVCSTARTUPTYPE=Automatic /BROWSERSVCSTARTUPTYPE=Disabled /RSSVCACCOUNT=$($ReportServiceAcct.UserName) /RSSVCPASSWORD=$($ReportServiceAcct.GetNetworkCredential().Password) /RSSVCSTARTUPTYPE=Automatic /FTSVCACCOUNT=NT Service\MSSQLFDLauncher /SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS /SQLSYSADMINACCOUNTS=$Using:SysAdmin /SECURITYMODE=SQL /SAPWD=$($SAPWDAcct.GetNetworkCredential().Password) /TCPENABLED=0 /NPENABLED=1" -wait -Credential $Using:Credential
+        
             start-Process -FilePath "$($DriveLetter):\Setup.exe" -ArgumentList $ArgumentList -wait -Credential $Using:Credential
           
             Dismount-DiskImage -ImagePath "c:\temp\$($Using:SQLISOFile.Name)" 
+
+            # ----- Take a look at the SQL install Summary Log file.  
+            # ----- Setting to silently continue.  I don't want to fail this cmdlet if it can't find the file.
+            Write-Output ( Get-Content "C:\Program Files\Microsoft SQL Server\120\Setup Bootstrap\Log\Summary.txt" )
             
         }
-
-        # ----- Take a look at the SQL install Summary Log file.  
- #       $SummaryLog =  Get-Content "\\$ComputerName\C$\Program Files\Microsoft SQL Server\120\Setup Bootstrap\Log"
-
- #       Write-Verbose $SummaryLog
 
         # ----- Turn off CredSSP
         Write-Verbose "Remove CredSSP Configure $ComputerName"
         Invoke-Command -ComputerName $ComputerName -ScriptBlock {
              Disable-WSMANCredSSP -Role Server | Out-Null
         }
+
+        # ----- Take a look at the SQL install Summary Log file.  
+        
+        # ----- Regex to get the exitcode
+        ($SummaryLog | Select-String -Pattern 'Exit error code:' ) -match '(\-?\d+)' | Out-Null
+        $ExitCode = $Matches[1]
+        $ExitCode
+
+        # ----- Regex to get the Exit Message
+        ($SummaryLog | Select-String -Pattern 'Exit message:' ) -match ':\s+(.+)' | Out-Null
+        $ExitMessage = $Matches[1]
+        $ExitMessage
+
+        $Result = New-Object -TypeName psobject -Property @{
+            'ExitCode' = $ExitCode
+            'ExitMessage' = $ExitMessage
+            'SummaryLog' = $SummaryLog
+        }
+
+        Write-Output $Result   
     }
 
     End {
         # ----- Clean up files
-     #   remove-item \\$ComputerName\c$\Temp\$($SQLISOFile.Name)
+        remove-item \\$ComputerName\c$\Temp\$($SQLISOFile.Name)
     }
 }
 
-$NetSVC = Get-Credential 'Network Service'
+$SQLSVCAccount = Get-Credential 'stratuslivedemo\sql.user'
+#$SQLSVCAccount = Get-Credential 'Network Service'
 
 $secpasswd = ConvertTo-SecureString '1$tellar$ervice' -AsPlainText -Force
 $CRMAdmin = New-Object System.Management.Automation.PSCredential (“stratuslivedemo\administrator”, $secpasswd)
@@ -235,10 +252,10 @@ $SAWPD = New-Object System.Management.Automation.PSCredential (“SA”, $secpas
 install-SQLServer -ComputerName JB-SQL01.stratuslivedemo.com `
     -SQLISO \\vaslnas.stratuslivedemo.com\StratusLive\Software\en_sql_server_2014_developer_edition_with_service_pack_1_x64_dvd_6668542.iso `
     -Features SQLEngine,FullText,RS,SSMS,ADV_SSMS `
-    -SQLAgent $NetSVC `
+    -SQLAgent $SQLSVCAccount `
     -SAPWD $SAWPD `
-    -SQLService $NetSVC `
+    -SQLService $SQLSVCAccount `
     -SysAdmin 'stratuslivedemo\jeff.buenting','stratuslivedemo\administrator' `
-    -ReportService $NetSVC `
+    -ReportService $SQLSVCAccount `
     -Credential $CRMAdmin `
     -verbose
